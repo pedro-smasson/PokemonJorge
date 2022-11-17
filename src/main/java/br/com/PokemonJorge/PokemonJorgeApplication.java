d1cfd4f4dd29e3e57f4bf021d8385bdaac0aaade
Pedro Masson <pedro.smasson@gmail.com>
2022-11-17 08:26:48 -0300

    first commit using spring

---
+++ src/main/java/br/com/PokemonJorge/PokemonJorgeApplication.java
@@ -0,0 +1,13 @@
+package br.com.PokemonJorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonJorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonJorgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/PokemonJorge/PokemonJorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.PokemonJorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonJorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
